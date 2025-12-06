6eb2d7b1693dd0406558eaf3150b2ac2b97f0834
Maksym Tovstolis <108622149+lifeconsciousness@users.noreply.github.com>
2025-12-06 19:59:47 +0100

    fix navigation

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
           <Waitlist />
         </>
       ) : (
-        <LearningPlatform />
+        <LearningPlatform onNavigate={setCurrentPage} />
       )}
     </div>
   )
